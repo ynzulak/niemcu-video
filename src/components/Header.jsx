7d0c68c26e13ec71f9c83b788387272e0b82cd50
yznulak <maksymilian.a.kaluzny@gmail.com>
2023-06-29 19:47:38 +0200

    Added desktop size header and improved scrollbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,49 +12,72 @@ function Header() {
 
     const toggleMenu = () => {
         setOpen(!isOpen);
-      };
+    };
 
     return (
 
-<header className='header'> 
-    <nav className='burger-menu'>
-        <img src={logoTitle} alt='Logo on the top of the site'/>
-        <div className='hamburger-bars'>
-        <Hamburger size={30} onClick={toggleMenu} toggled={isOpen} toggle={setOpen} />
-        </div>
-        <CSSTransition
-        in={isOpen}
-        timeout={300}
-        classNames="menu"
-        unmountOnExit
-      >
-        <div className='menu'>
-            <ul>
-                <li>
-                    <Link to='/' onClick={toggleMenu}>Strona główna</Link>
-                </li>
-                <li>
-                    <Link to='/zdjecia' onClick={toggleMenu}>Zdjęcia</Link>
-                </li>
-                <li>
-                    <Link to='/filmy' onClick={toggleMenu}>Filmy</Link>
-                </li>
-                <li>
-                    <Link to='/lektor' onClick={toggleMenu}>Lektor</Link>
-                </li>
-                <li>
-                    <Link to='/grafika' onClick={toggleMenu}>Grafika</Link>
-                </li>
-                <li>
-                    <Link to='/kontakt' onClick={toggleMenu}>Kontakt</Link>
-                </li>
-            </ul>
-        </div>
-        </CSSTransition>
-    </nav>
-</header>
+        <header className='header'>
+            <nav className='burger-menu'>
+                <img src={logoTitle} alt='Logo on the top of the site' />
+                <div className='hamburger-bars'>
+                    <Hamburger size={30} onClick={toggleMenu} toggled={isOpen} toggle={setOpen} />
+                </div>
+                <CSSTransition
+                    in={isOpen}
+                    timeout={300}
+                    classNames="menu"
+                    unmountOnExit
+                >
+                    <div className='menu'>
+                        <ul>
+                            <li>
+                                <Link to='/' onClick={toggleMenu}>Strona główna</Link>
+                            </li>
+                            <li>
+                                <Link to='/zdjecia' onClick={toggleMenu}>Zdjęcia</Link>
+                            </li>
+                            <li>
+                                <Link to='/filmy' onClick={toggleMenu}>Filmy</Link>
+                            </li>
+                            <li>
+                                <Link to='/lektor' onClick={toggleMenu}>Lektor</Link>
+                            </li>
+                            <li>
+                                <Link to='/grafika' onClick={toggleMenu}>Grafika</Link>
+                            </li>
+                            <li>
+                                <Link to='/kontakt' onClick={toggleMenu}>Kontakt</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </CSSTransition>
+                <div className='menu-big'>
+                        <ul>
+                            <li>
+                                <Link to='/'>Strona główna</Link>
+                            </li>
+                            <li>
+                                <Link to='/zdjecia'>Zdjęcia</Link>
+                            </li>
+                            <li>
+                                <Link to='/filmy'>Filmy</Link>
+                            </li>
+                            <li>
+                                <Link to='/lektor'>Lektor</Link>
+                            </li>
+                            <li>
+                                <Link to='/grafika'>Grafika</Link>
+                            </li>
+                            <li>
+                                <Link to='/kontakt'>Kontakt</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    
+            </nav>
+        </header>
 
     );
-  }
-  
-  export default Header;
+}
+
+export default Header;
